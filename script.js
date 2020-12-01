4c116b24a722b2cbcb7182e32ff1c1199cb0a0dc
Sheshu <reddysheshasena@gmail.com>
2020-12-02 02:40:30 +0530

    added script and functionality

---
+++ script.js
@@ -0,0 +1,17 @@
+let a;
+let time,date;
+
+setInterval(() => {
+     a = new Date();
+     time=a.toLocaleTimeString();
+     date=a.toLocaleDateString();
+document.querySelector("#print").innerHTML=time+"<br> Date: "+date +" (IST)";
+}, 1000);
+
+let btn=document.querySelector("#load");
+btn.addEventListener("click",reload);
+
+function reload()
+{
+    window.location.reload();
+}
